117991a3440d2fd1cefbd5d4876b65ead442062f
The Dod <unclezzzen@gmail.com>
2013-04-13 20:45:38 +0700

    fork button always visible

--- bl.ocks.js
+++ bl.ocks.js
@@ -1,10 +1,11 @@
 var urlparts = location.toString().split('/');
 if (urlparts[2]==='bl.ocks.org') {
   var gist = urlparts[5];
+  document.write('<p align="center">');
   if (window===window.top) {
-      document.write('<p align="center">Brought to you via <a target="_top" href="http://bl.ocks.org/'+gist+'/">bl.ocks.org</a>.');
-      document.write('<a target="_top" class="button centered nofloat" href="https://gist.github.com/'+gist+'/">Fork this</a></p>');
+      document.write('Brought to you via <a target="_top" href="http://bl.ocks.org/'+gist+'/">bl.ocks.org</a>.');
   } else {
-      document.write('<p align="center"><a target="_top" class="button centered nofloat" href="'+location.toString()+'">View full screen</a></p>');
+      document.write('<a target="_top" class="button centered" href="'+location.toString()+'">View full screen</a> ');
   }
+  document.write('<a target="_top" class="button centered nofloat" href="https://gist.github.com/'+gist+'/">Fork this</a></p>');
 }
