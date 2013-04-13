5856b5a491d1a14815ecaf30be6958a27c506af0
The Dod <unclezzzen@gmail.com>
2013-04-13 13:58:27 +0700

    Show only relevant bl.ocks button

--- bl.ocks.js
+++ bl.ocks.js
@@ -1,6 +1,9 @@
 var urlparts = location.toString().split('/');
 if (urlparts[2]==='bl.ocks.org') {
   var gist = urlparts[5];
-  document.write('<a target="_top" class="button centered nofloat" href="http://bl.ocks.org/'+gist+'/">Code</a> ');
-  document.write('<a target="_top" class="button centered nofloat" href="'+location.toString()+'">Full screen</a>');
+  if (window===window.top) {
+      document.write('<p align="center"><a target="_top" class="button centered nofloat" href="http://bl.ocks.org/'+gist+'/">View source at bl.ocks.org</a></p>');
+  } else {
+      document.write('<p align="center"><a target="_top" class="button centered nofloat" href="'+location.toString()+'">View full screen</a></p>');
+  }
 }
