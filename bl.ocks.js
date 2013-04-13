f6827fe2e8c8e51aa27db7e3202ee999933ff8f2
The Dod <unclezzzen@gmail.com>
2013-04-13 14:00:06 +0700

    Shorter button text

--- bl.ocks.js
+++ bl.ocks.js
@@ -2,7 +2,7 @@ var urlparts = location.toString().split('/');
 if (urlparts[2]==='bl.ocks.org') {
   var gist = urlparts[5];
   if (window===window.top) {
-      document.write('<p align="center"><a target="_top" class="button centered nofloat" href="http://bl.ocks.org/'+gist+'/">View source at bl.ocks.org</a></p>');
+      document.write('<p align="center"><a target="_top" class="button centered nofloat" href="http://bl.ocks.org/'+gist+'/">View source</a></p>');
   } else {
       document.write('<p align="center"><a target="_top" class="button centered nofloat" href="'+location.toString()+'">View full screen</a></p>');
   }
