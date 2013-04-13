95fb7b2bd64103f2705659e19ec6b9c4c032c19d
The Dod <unclezzzen@gmail.com>
2013-04-13 20:50:37 +0700

    Cosmetics

--- bl.ocks.js
+++ bl.ocks.js
@@ -1,11 +1,11 @@
 var urlparts = location.toString().split('/');
 if (urlparts[2]==='bl.ocks.org') {
   var gist = urlparts[5];
-  document.write('<p align="center">');
+  document.write('<a target="_top" class="button centered" href="https://gist.github.com/'+gist+'/">Fork this</a> ');
   if (window===window.top) {
       document.write('Brought to you via <a target="_top" href="http://bl.ocks.org/'+gist+'/">bl.ocks.org</a>.');
   } else {
-      document.write('<a target="_top" class="button centered" href="'+location.toString()+'">View full screen</a> ');
+      document.write('<a target="_top" class="button centered" href="'+location.toString()+'">View full screen</a>');
   }
-  document.write('<a target="_top" class="button centered nofloat" href="https://gist.github.com/'+gist+'/">Fork this</a></p>');
+  <div class="clear"></div>
 }
