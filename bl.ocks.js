acbf45b74daf2cd5754b6edfaad0c5dc311be6d3
The Dod <unclezzzen@gmail.com>
2013-04-13 14:11:15 +0700

    Credit to bl.ocks in full screen, link to gist.

--- bl.ocks.js
+++ bl.ocks.js
@@ -2,7 +2,8 @@ var urlparts = location.toString().split('/');
 if (urlparts[2]==='bl.ocks.org') {
   var gist = urlparts[5];
   if (window===window.top) {
-      document.write('<p align="center"><a target="_top" class="button centered nofloat" href="http://bl.ocks.org/'+gist+'/">View source</a></p>');
+      document.write('<p align="center">Brought to you via <p align="center"><a target="_top" href="https://bl.ocks.org/'+gist+'/">bl.ocks.org</a>.');
+      document.write('<a target="_top" class="button centered nofloat" href="https://gist.github.com/'+gist+'/">Fork this</a></p>');
   } else {
       document.write('<p align="center"><a target="_top" class="button centered nofloat" href="'+location.toString()+'">View full screen</a></p>');
   }
