2fb7afcb4ca64dd6dd157988138648deadc4fb79
The Dod <unclezzzen@gmail.com>
2013-04-13 20:57:07 +0700

    cosmetics

--- bl.ocks.js
+++ bl.ocks.js
@@ -3,9 +3,9 @@ if (urlparts[2]==='bl.ocks.org') {
   var gist = urlparts[5];
   document.write('<a target="_top" class="button centered" href="https://gist.github.com/'+gist+'/">Fork this</a> ');
   if (window===window.top) {
-      document.write('Brought to you via <a target="_top" href="http://bl.ocks.org/'+gist+'/">bl.ocks.org</a>.');
+    document.write('<div class="clear">Brought to you via <a target="_top" href="http://bl.ocks.org/'+gist+'/">bl.ocks.org</a>.</a></div>');
   } else {
-      document.write('<a target="_top" class="button centered" href="'+location.toString()+'">View full screen</a>');
+    document.write('<a target="_top" class="button centered" href="'+location.toString()+'">View full screen</a>');
+    document.write('<div class="clear"></div>');
   }
-  document.write('<div class="clear"></div>');
 }
