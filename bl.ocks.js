059364bdd033800a725d22aacda2c260c68c5cb0
The Dod <unclezzzen@gmail.com>
2013-04-13 13:28:54 +0700

    typo fix

--- bl.ocks.js
+++ bl.ocks.js
@@ -1,6 +1,6 @@
 var urlparts = location.toString().split('/');
 if (urlparts[2]==='bl.ocks.org') {
   var gist = urlparts[5];
-  document.write('<a target="_top" class="button" href="http://bl.ocks.org/'+gist'+'/">Code</a> ');
-  document.write('<a target="_top" class="button" href="http://bl.ocks.org/raw/'+gist'+'/">Full screen</a><hr>');
+  document.write('<a target="_top" class="button" href="http://bl.ocks.org/'+gist+'/">Code</a> ');
+  document.write('<a target="_top" class="button" href="http://bl.ocks.org/raw/'+gist+'/">Full screen</a><hr>');
 }
